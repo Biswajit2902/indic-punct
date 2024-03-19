c3470de9f9cbe4c138f50116f8e63150ef08f048
Biswajit Satapathy <biswajit2902@gmail.com>
2024-03-19 13:55:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+sudo apt-get update && apt-get install -y libsndfile1 ffmpeg python3-dev
 pip install Cython
 sudo apt-get install libgmp-dev build-essential
 # conda install libgcc gmp
