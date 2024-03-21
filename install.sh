e873a1a672b95167259b674bfae2227a003c9c13
Biswajit Satapathy <biswajit2902@gmail.com>
2024-03-21 14:26:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ sudo apt-get update && apt-get install -y libsndfile1 ffmpeg python3-dev
 pip install Cython
 sudo apt-get install libgmp-dev build-essential
 # conda install libgcc gmp
-pip install nemo_toolkit
-#['all']==v1.0.2
+pip install nemo_toolkit['all'] #==v1.0.2
 
 # conda install -c conda-forge pynini==2.1.4
